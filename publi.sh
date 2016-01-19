a102b5436a9f0884a4f1ab1fa7890b06bda843c3
Michael Ficarra <github@michael.ficarra.me>
2016-01-19 09:48:53 -0800

    Create .travis.yml; auto-publish on successful CI of master branch

--- publi.sh
+++ publi.sh
@@ -1,11 +1,25 @@
-currentbranch=$(git rev-parse --abbrev-ref HEAD)
-branch=$(uuidgen)
-git checkout --orphan "$branch"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+TEMP_BRANCH=$(date | md5sum | cut -d ' ' -f 1)
+if [ "$TRAVIS" == true ]; then
+  CURRENT_BRANCH="$TRAVIS_BRANCH"
+  [ "$TRAVIS_SECURE_ENV_VARS" == true -a -n "$GITHUB_DEPLOY_KEY" -a "$TRAVIS_PULL_REQUEST" == false -a "$TRAVIS_BRANCH" == master ] || exit 1
+  openssl aes-256-cbc -K $encrypted_1cf5552cca3f_key -iv $encrypted_1cf5552cca3f_iv -in github-deploy-key.enc -out github-deploy-key -d
+  chmod 600 github-deploy-key
+  eval "$(ssh-agent -s)"
+  ssh-add github-deploy-key
+  rm github-deploy-key
+  git remote set-url --push origin "git@github.com:$TRAVIS_REPO_SLUG.git"
+  git config --global user.email "contact@travis-ci.com"
+  git config --global user.name "Travis CI"
+  echo "Publishing to gh-pages"
+fi
+set -x
+git checkout --orphan "$TEMP_BRANCH"
 npm install
 make
 git reset .
 git add -f index.html ecmarkup.{js,css}
 git commit -m 'gh-pages'
 git push -f origin HEAD:gh-pages
-git checkout -f "$currentbranch"
-git branch -D "$branch"
+git checkout -f "$CURRENT_BRANCH"
+git branch -D "$TEMP_BRANCH"
